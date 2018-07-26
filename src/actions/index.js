d47925ec16e4c593a0856e582a587d547d0f4791
Scott Rizzo <jkarizzo345@yahoo.com>
2018-07-26 13:34:27 -0700

    second commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,9 +5,9 @@ export const FETCH_POSTS = 'fetch_posts';
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?RIZZO1234';
 
-export function fetchPost() {
+export function fetchPosts() {
    const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
-   
+
    return {
       type: FETCH_POSTS,
       payload: request
